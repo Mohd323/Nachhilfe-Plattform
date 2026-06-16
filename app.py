c0f928586979a5d63e573975f0439a8b152ac90e
Emrah Rabotic <raboticemrah538@gmail.com>
2026-06-16 17:08:12 +0200

    Login und Registrierung erstellt

--- app.py
+++ app.py
@@ -7,6 +7,13 @@ app.secret_key = 'nachhilfe-geheim-123'
 @app.route('/')                                 # wenn jemand die Startseite aufruft (/), führe die Funktion darunter aus
 def startseite():                               # das ist die Python-Funktion für die Startseite
     return render_template('startseite.html')   # Flask sucht die Datei startseite.html im templates/ Ordner und schickt sie an den Browser
+@app.route('/login')
+def login():                                    
+     return render_template('login.html')
 
+@app.route('/register')
+def register():
+    return render_template('register.html')
 if __name__ == '__main__':                      # startet die App nur wenn du sie direkt ausführst
     app.run(debug=True)                         # startet den Webserver, debug=True zeigt Fehlermeldungen direkt im Browser
+    
