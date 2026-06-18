615e7c47368d16743b2d01bf51d892c539e1691d
aldarwish24 <s_aldarwish24@stud.hwr-berlin.de>
2026-06-19 01:04:33 +0200

    Lehrerprofil-Tabelle

--- models.py
+++ models.py
@@ -22,3 +22,14 @@ class SchülerProfil(db.Model):
     klasse = db.Column(db.String(50))
     ausweis_dokument_url = db.Column(db.Text, nullable=True)
     verifizierungs_status = db.Column(db.String(30), nullable=False, default="ausstehend")
+
+
+# Lehrerprofil
+class LehrerProfil(db.Model):
+    __tablename__ = "lehrer_profile"
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    beschreibung = db.Column(db.Text)
+    stundenpreis = db.Column(db.Float)
+
