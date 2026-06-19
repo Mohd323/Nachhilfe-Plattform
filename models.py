f57a52969dbc061c4f724713c8f1464a0bbae226
Mohd Alkhtib <s_alkhtib24@stud.hwr-berlin.de>
2026-06-19 02:39:48 +0200

    Fehler (Lösung: lehrer_profil.id ersetzt durch lehrer_profile.id)

--- models.py
+++ models.py
@@ -54,7 +54,7 @@ class LehrerFach(db.Model):
     __tablename__ = "lehrer_fach"
 
     id = db.Column(db.Integer, primary_key=True)
-    lehrer_profil_id = db.Column(db.Integer, db.ForeignKey("lehrer_profil.id"), nullable=False)
+    lehrer_profil_id = db.Column(db.Integer, db.ForeignKey("lehrer_profile.id"), nullable=False)
     fach_id = db.Column(db.Integer, db.ForeignKey("fach.id"), nullable=False)
     klassenstufen = db.Column(db.String(50), nullable=True)
     
@@ -63,7 +63,7 @@ class Verfügbarkeit(db.Model):
     __tablename__ = "verfügbarkeit"
 
     id = db.Column(db.Integer, primary_key=True)
-    lehrer_profil_id = db.Column(db.Integer, db.ForeignKey("lehrer_profil.id"), nullable=False)
+    lehrer_profil_id = db.Column(db.Integer, db.ForeignKey("lehrer_profile.id"), nullable=False)
     wochentag = db.Column(db.String(20), nullable=False)
     von_uhrzeit = db.Column(db.Time, nullable=False)
     bis_uhrzeit = db.Column(db.Time, nullable=False)
@@ -73,7 +73,7 @@ class Verifizierungsdokument(db.Model):
     __tablename__ = "verifizierungsdokument"
 
     id = db.Column(db.Integer, primary_key=True)
-    lehrer_profil_id = db.Column(db.Integer, db.ForeignKey("lehrer_profil.id"), nullable=False)
+    lehrer_profil_id = db.Column(db.Integer, db.ForeignKey("lehrer_profile.id"), nullable=False)
     dokument_typ = db.Column(db.String(50), nullable=False)
     datei_url = db.Column(db.Text, nullable=False)
     status = db.Column(db.String(30), nullable=False, default="ausstehend")
