6f05d63d65870ef261dff85ea976db226417ba3f
EmrahRabotic <s_rabotic24@stud.hwr-berlin.de>
2026-06-19 16:38:57 +0200

    Klasse RegisterForm für die Benutzerregistrierung hinzufügen

--- forms.py
+++ forms.py
@@ -1,3 +1,15 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SelectField, SubmitField
 from wtforms.validators import DataRequired, Email, Length
+
+class RegisterForm(FlaskForm):
+    vorname = StringField("Vorname", validators=[DataRequired()])
+    nachname = StringField("Nachname", validators=[DataRequired()])
+    email = StringField("E-Mail", validators=[DataRequired(), Email()])
+    passwort = PasswordField("Passwort", validators=[DataRequired(), Length(min=6)])
+    rolle = SelectField(
+        "Rolle",
+        choices=[("schueler", "Schüler/in"), ("lehrer", "Lehrer/in")],
+        validators=[DataRequired()]
+    )
+    submit = SubmitField("Registrieren")
